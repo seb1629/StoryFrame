b8e032ee0cdd1be6f2bc2b10f4fcfdc7714c4d56
SEBASTIEN MONTIBELLER <montycoder@gmail.com>
2016-06-13 12:54:16 +1000

    First Screen Created

---
+++ StoryFrame/LogInViewController.swift
@@ -0,0 +1,25 @@
+//
+//  LogInViewController.swift
+//  StoryFrame
+//
+//  Created by SEBASTIEN MONTIBELLER on 13/06/2016.
+//  Copyright © 2016 SEBASTIEN MONTIBELLER. All rights reserved.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        navigationController?.navigationBarHidden = true
+    }
+    
+
+
+}
---
+++ StoryFrame/ScaleImage.swift
@@ -0,0 +1,24 @@
+//
+//  ScaleImage.swift
+//  StoryFrame
+//
+//  Created by SEBASTIEN MONTIBELLER on 13/06/2016.
+//  Copyright © 2016 SEBASTIEN MONTIBELLER. All rights reserved.
+//
+
+
+import UIKit
+
+extension UIImage {
+    
+    class func scaleImageToSize(img: UIImage, size: CGSize) -> UIImage {
+        UIGraphicsBeginImageContext(size)
+        
+        img.drawInRect(CGRect(origin: CGPointZero, size: size))
+        
+        let scaledImage = UIGraphicsGetImageFromCurrentImageContext()
+        
+        UIGraphicsEndImageContext()
+        return scaledImage
+    }
+}
